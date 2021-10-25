93571dc257f5476ee5d91ccb0abdfdfa97379e6b
smohri-michibiki <s.mohri@michibiki.io>
2021-10-25 22:33:46 +0900

    fix: 実行ユーザ (go) に複数の GROUP_ID を指定できるようにする修正を改善した #13 USERMAP_GIDを廃止しUSERMAP_GIDSにまとめる

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,16 @@ set -euo pipefail
 
 map_uidgid() {
     USERMAP_ORIG_UID=$(id -u go)
-    USERMAP_ORIG_GID=$(id -g go)
     USERMAP_ORIG_GIDS=$(id -G go)
     USERMAP_UID=${USERMAP_UID:-$USERMAP_ORIG_UID}
-    USERMAP_GID=${USERMAP_GID:-$USERMAP_ORIG_GID}
     USERMAP_GIDS=${USERMAP_GIDS:-$USERMAP_ORIG_GIDS}
-    if [ "${USERMAP_UID}" != "${USERMAP_ORIG_UID}" ] || [ "${USERMAP_GID}" != "${USERMAP_ORIG_GID}" ] || [ "${USERMAP_GIDS}" != "${USERMAP_ORIG_GIDS}" ]; then
-        echo "Starting with UID : $USERMAP_UID, GID: $USERMAP_GID, GIDs: $USERMAP_GIDS"
+    if [ "${USERMAP_UID}" != "${USERMAP_ORIG_UID}" ] || [ "${USERMAP_GIDS}" != "${USERMAP_ORIG_GIDS}" ]; then
+        echo "Starting with UID : $USERMAP_UID, GID: $USERMAP_GIDS"
+        USERMAP_GIDS=($USERMAP_GIDS);
         usermod -u $USERMAP_UID -o go
-        groupmod -g $USERMAP_GID go
-        for i in $USERMAP_GIDS; do
-            if [ $USERMAP_GID -ne $i ]; then
+        groupmod -g ${USERMAP_GIDS[0]} go
+        for i in ${USERMAP_GIDS[@]}; do
+            if [ ${USERMAP_GIDS[0]} -ne $i ]; then
                 groupadd -g $i $i;
                 usermod -aG $i go
             fi
